2d5ca5e20d1983e0ab2634d6f1f812e13e4e952e
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-03-05 22:24:57 +0700

    add admin page

---
+++ CoffeeMachine/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeMachine.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
